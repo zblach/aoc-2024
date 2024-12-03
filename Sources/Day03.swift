1cad1382262d36212cf219e907ce02a2272ab246
Zak Blacher <github@chalkzebra.com>
2024-12-02 23:20:19 -0800

    refinement

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -2,41 +2,39 @@ import Algorithms
 import RegexBuilder
 
 struct Day03: AdventDay {
-  // Save your data in a corresponding text file in the `Data` directory.
   var data: String
 
   func part1() -> Any {
-    return data.matches(of: /mul\((\d+),(\d+)\)/).reduce(0) { acc, m in
+    let mulOp: Regex<(Substring, Substring, Substring)> = /mul\((\d+),(\d+)\)/
+    return data.matches(of: mulOp).reduce(0) { acc, m in
       acc + (Int(m.1)! * Int(m.2)!)
     }
   }
 
   func part2() -> Any {
-    let doFlag: Regex<Substring> = /do\(\)/
-    let dontFlag: Regex<Substring> = /don't\(\)/
+    let doStr = "do()"
+    let dontStr = "don't()"
     let mulOp: Regex<(Substring, Substring, Substring)> = /mul\((\d+),(\d+)\)/
 
-    return data.matches(
-      of: Regex {
-        ChoiceOf {
-          doFlag
-          dontFlag
-          mulOp
-        }
+    let pattern = Regex {
+      ChoiceOf {
+        Regex { doStr }
+        Regex { dontStr }
+        mulOp
       }
-    ).reduce((enabled: true, acc: 0)) { result, match in
+    }
+
+    return data.matches(of: pattern).reduce((enabled: true, acc: 0)) { result, match in
       switch match.0 {
-      case "do()":
+      case doStr:  // do
         return (true, result.acc)
-      case "don't()":
+      case dontStr:  // do not
         return (false, result.acc)
-      case let mul:
-        if let (_, n1, n2) = try? mulOp.wholeMatch(in: mul)?.output {
-          let a = Int(n1)!
-          let b = Int(n2)!
-          return (result.enabled, result.enabled ? result.acc + (a * b) : result.acc)
-        }
-        return result
+      default:  // there is no try
+        return (
+          result.enabled,
+          result.enabled ? result.acc + (Int(match.1!)! * Int(match.2!)!) : result.acc
+        )
       }
     }.acc
   }
