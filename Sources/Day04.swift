0377c5dbdb0d09f64c8220b2d11b42a3d45f6102
Zak Blacher <github@chalkzebra.com>
2024-12-04 08:11:50 -0800

    collapsed guards, centered pattern around 'A'

--- Sources/Day04.swift
+++ Sources/Day04.swift
@@ -33,7 +33,6 @@ struct Day04: AdventDay {
     return product(0..<height, 0..<width).reduce(0) { sum, pos in
       sum + directions.count { delta in checkPattern(word: "XMAS", cell: pos, delta: delta) }
     }
-
   }
 
   func part2() -> Any {
@@ -43,27 +42,22 @@ struct Day04: AdventDay {
     let ms: Set<Character> = ["M", "S"]
 
     // Check each possible 3x3 grid position
-    return product(0..<(height - 2), 0..<(width - 2)).count { row, col in
+    return product(1..<(height - 1), 1..<(width - 1)).count { row, col in
       // Check if this 3x3 grid matches the pattern:
       // a.b
       // .A.
       // c.d
 
       // 'A' is a literal A. a-d are the corners that must be either M or S.
-      guard entities[row + 1][col + 1] == "A" else { return false }
+      guard entities[row][col] == "A" else { return false }
 
-      let a = entities[row][col]
-      let b = entities[row][col + 2]
-      let c = entities[row + 2][col]
-      let d = entities[row + 2][col + 2]
+      let a = entities[row - 1][col - 1]
+      let b = entities[row - 1][col + 1]
+      let c = entities[row + 1][col - 1]
+      let d = entities[row + 1][col + 1]
 
-      // Check all corners are either M or S.
-      guard [a, b, c, d].allSatisfy(ms.contains) else { return false }
-
-      // Check that diagonals differ. MAM and SAS aren't allowed.
-      guard a != d, b != c else { return false }
-
-      return true
+      // Check all corners are either M or S, but opposite corners must be different.
+      return [a, b, c, d].allSatisfy(ms.contains) && a != d && b != c
     }
   }
 }
